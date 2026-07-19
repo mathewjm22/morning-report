192f482db6f284fcbebf7b9acb717cc86dd3dccd
Jacob Mathew <75663388+mathewjm22@users.noreply.github.com>
2026-07-18 23:47:50 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// IMPORTANT: set `base` to your GitHub Pages subpath.
-// If your repo is https://github.com/USER/morning-report, base = '/morning-report/'
-// For a custom domain or user page, use '/'.
 export default defineConfig({
   plugins: [react()],
   base: '/morning-report/',
+  build: { outDir: 'dist' },
 });
