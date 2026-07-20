eb0891c59cc4cb92594bb0c1b98dafd3bc6c574e
Jacob Mathew <75663388+mathewjm22@users.noreply.github.com>
2026-07-19 20:29:54 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,23 @@
-/** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,jsx}'],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        sage: {
+          50:  '#f4f7f4',
+          100: '#e5ede4',
+          200: '#c9dbc8',
+          300: '#a3c1a3',
+          400: '#78a17a',
+          500: '#5a865e',
+          600: '#456b49',
+          700: '#37553b',
+          800: '#2d4530',
+          900: '#263a29',
+          950: '#131f15',
+        },
+      },
+    },
+  },
   plugins: [],
 };
